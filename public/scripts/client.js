362390ba1ca2b4750e018dc0739300e0f6d0096a
Warda Nur <warduhh@gmail.com>
2023-04-06 22:46:01 +0000

    Added event handler, added function to prevent tweet duplication

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,28 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
   //toggles the text area to appear when you click on 'Write a new tweet'
-  $(".angles-down").on("click", function() {
+  $(".chevron").on("click", function () {
     $('.new-tweet').toggle();
     $('#tweet-text').focus();
   });
 
   //escape function takes in a string and sets the HTML contained within the element to to prevent XSS 
-  const escape = function(str) {
+  const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
   //createTweetElement function takes in a tweet object and is responsible for returning a tweet
-  const createTweetElement = function(tweet) {
+  const createTweetElement = function (tweet) {
     // element containing the entire HTML structure of the tweet
-    const $tweet = `<div class="box">
+    const $tweet = `
+     <div class="box">
     <article class="article">
     <header class="old-tweet-header">
       <span class="name-container">
-        <img class="avatar-container" src="${tweet.user.avatars}">
+        <img class="avatar-container" 
+        src="${tweet.user.avatars}">
         <p class="name">${tweet.user.name}</p>
       </span>
       <p class="handle">${tweet.user.handle}</p>
@@ -45,7 +47,7 @@ $(document).ready(function() {
   };
 
   //renderTweets function takes in an array of tweet objects and then appending each one to the container
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     // loops through tweets
     for (let tweet of tweets) {
       // calls createTweetElement for each tweet
@@ -55,12 +57,14 @@ $(document).ready(function() {
     }
   };
 
-  // event listener that listens for the submit event
-  $('form').submit(function(event) {
-    event.preventDefault();
-    const text = $('#tweet-text').val();
+  const renderLatestTweet = function (tweets) {
+    let lastTweet = tweets[tweets.length - 1];
+    const $lastTweetElement = createTweetElement(lastTweet);
+    $('.old-tweets').prepend($lastTweetElement);
+  };
 
-    //Form Validation
+
+  function handlingTextError(text) {
     if (!text) {
       $('.new-tweet-error').text("❗Text is empty❗");
       $('.new-tweet-error').slideDown();
@@ -69,21 +73,42 @@ $(document).ready(function() {
       $('.new-tweet-error').slideDown();
     } else {
       $('.new-tweet-error').slideUp();
-
-      // Serializes the form data
-      const data = $(this).serialize();
-      //sends the form data to the server as a query string.
-      $.ajax({
-        type: "POST",
-        url: "/tweets",
-        data: data,
-      }).then(() => {
-        $('#tweet-text').val("");
-        $('.counter').text(140);
-        loadTweets();
-      });
     }
+  }
+  // event listener that listens for when text changes
+  $('#tweet-text').on("input", function () {
+    const text = $(this).val();
+    handlingTextError(text);
   });
+
+  // event listener that listens for the submit event
+  $('form').submit(function (event) {
+    event.preventDefault();
+    const text = $(`#tweet-text`).val();
+    handlingTextError(text);
+    // Serializes the form data
+    const data = $(this).serialize();
+    //sends the form data to the server as a query string.
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: data,
+    }).then(() => {
+      //clears the textarea and resets the counter
+      $('#tweet-text').val("");
+      $('.counter').text(140);
+      loadLatestTweet();
+    });
+  });
+
+  const loadLatestTweet = () => {
+    $.ajax({
+      url: "/tweets",
+      success: "success",
+    }).then((tweets) => {
+      renderLatestTweet(tweets);
+    });
+  };
 
   // loadtweets function will use jQuery to make a request to /tweets and receive the array of tweets as JSON.
   const loadTweets = () => {
@@ -91,6 +116,7 @@ $(document).ready(function() {
       url: "/tweets",
       success: "success",
     }).then((tweets) => {
+      loadedTweets = tweets;
       renderTweets(tweets);
     });
   };
